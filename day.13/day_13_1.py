14ba80ce762367ad1f90b37a7e55f7b4d7427dd4
Tan Wang Leng <tanwangleng@outlook.com>
2018-12-17 16:56:45 +0800

    Add day.13 (part 1)

---
+++ day.13/day_13_1.py
@@ -0,0 +1,134 @@
+import sys
+import functools
+
+# direction: 0 - north, 1 - east, 2 - south, 3 - west
+# intersectMemory: 0 - left, 1 - straight, 2 - right
+class Cart:
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.intersectMemory = 0
+
+    def get_pos_string(self):
+        return get_string(self.x, self.y)
+
+    def move_forward(self, forward_slash, backward_slash, intersections):
+        if self.direction == 0:
+            self.y -= 1
+        elif self.direction == 1:
+            self.x += 1
+        elif self.direction == 2:
+            self.y += 1
+        elif self.direction == 3:
+            self.x -= 1
+        
+        pos_str = self.get_pos_string()
+
+        if pos_str in forward_slash:
+            if self.direction == 0 or self.direction == 2:
+                self.turn_right()
+            else:
+                self.turn_left()
+        elif pos_str in backward_slash:
+            if self.direction == 0 or self.direction == 2:
+                self.turn_left()
+            else:
+                self.turn_right()
+        elif pos_str in intersections:
+            if self.intersectMemory == 0:
+                self.turn_left()
+            elif self.intersectMemory == 1:
+                # go straight - do nothing
+                pass
+            else:
+                self.turn_right()
+
+            self.intersectMemory = (self.intersectMemory + 1) % 3
+    
+    def is_in_another_cart(self, carts):
+        for c in carts:
+            if c == self:
+                continue
+            if c.x == self.x and c.y == self.y:
+                return True
+        return False
+    
+    def turn_right(self):
+        self.direction = (self.direction + 1) % 4
+    
+    def turn_left(self):
+        self.direction = (self.direction - 1) % 4
+
+
+def compare_cart_positions(cart_a, cart_b):
+    if cart_a.y == cart_b.y:
+        return cart_a.x - cart_b.x
+    return cart_a.y - cart_b.y
+
+
+# def get_x_y(string):
+#    return [int(x) for x in string.split(',')]
+
+
+def get_string(x, y):
+    return str(x) + ',' + str(y)
+
+
+def find_all_characters(string, character):
+    result = []
+
+    i = string.find(character)
+    while i != -1:
+        result.append(i)
+        i = string.find(character, i + 1)
+    
+    return result
+
+
+def parse_input(forward_slash, backward_slash, intersections, carts):
+    y = 0
+    for line in sys.stdin:
+        
+        for x in find_all_characters(line, '/'):
+            forward_slash.append(get_string(x, y))
+
+        for x in find_all_characters(line, '\\'):
+            backward_slash.append(get_string(x, y))
+
+        for x in find_all_characters(line, '+'):
+            intersections.append(get_string(x, y))
+
+        for x in find_all_characters(line, '^'):
+            carts.append(Cart(x, y, 0))
+
+        for x in find_all_characters(line, '>'):
+            carts.append(Cart(x, y, 1))
+
+        for x in find_all_characters(line, 'v'):
+            carts.append(Cart(x, y, 2))
+
+        for x in find_all_characters(line, '<'):
+            carts.append(Cart(x, y, 3))
+
+        y += 1
+
+
+def main():
+    forward_slash = []
+    backward_slash = []
+    intersections = []
+    carts = []
+
+    parse_input(forward_slash, backward_slash, intersections, carts)
+
+    while True:
+        carts = sorted(carts, key=functools.cmp_to_key(compare_cart_positions))
+        for c in carts:
+            c.move_forward(forward_slash, backward_slash, intersections)
+            if c.is_in_another_cart(carts):
+                print(str(c.x) + ',' + str(c.y))
+                return
+
+if __name__ == '__main__':
+    main()
